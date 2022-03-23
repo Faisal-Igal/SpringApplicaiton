c120d1dd1ab379f659e17806b2878ba82dadf841
faisal <faisalabdi101@gmail.com>
2022-03-22 19:16:58 -0700

    Created vehicle model

--- cigeapp/src/main/java/com/cigelogistics/cigeapp/controllers/VehicleModelController.java
+++ cigeapp/src/main/java/com/cigelogistics/cigeapp/controllers/VehicleModelController.java
@@ -2,6 +2,8 @@ package com.cigelogistics.cigeapp.controllers;
 
 import java.util.Optional;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
