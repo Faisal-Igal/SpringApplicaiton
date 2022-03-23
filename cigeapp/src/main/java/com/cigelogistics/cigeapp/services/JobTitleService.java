30c14072b7adc3e6d35d79bba7060f21c056aa8a
faisal <faisalabdi101@gmail.com>
2022-03-22 19:39:38 -0700

    Job title service

--- cigeapp/src/main/java/com/cigelogistics/cigeapp/services/JobTitleService.java
+++ cigeapp/src/main/java/com/cigelogistics/cigeapp/services/JobTitleService.java
@@ -1,5 +1,37 @@
 package com.cigelogistics.cigeapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cigelogistics.cigeapp.models.JobTitle;
+import com.cigelogistics.cigeapp.repositories.JobTitleRepository;
+
+@Service
 public class JobTitleService {
 
+	@Autowired
+	private JobTitleRepository jobTitleRepository;
+	
+	//Get All JobTitles
+	public List<JobTitle> findAll(){
+		return jobTitleRepository.findAll();
+	}	
+	
+	//Get JobTitle By Id
+	public Optional<JobTitle> findById(int id) {
+		return jobTitleRepository.findById(id);
+	}	
+	
+	//Delete JobTitle
+	public void delete(int id) {
+		jobTitleRepository.deleteById(id);
+	}
+	
+	//Update JobTitle
+	public void save(JobTitle jobTitle) {
+		jobTitleRepository.save(jobTitle);
+	}
 }
