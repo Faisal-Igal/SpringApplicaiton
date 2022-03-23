95d16488ef176d6652c0aa74cd526e4858e390db
faisal <faisalabdi101@gmail.com>
2022-03-22 19:41:07 -0700

    Created location service

--- cigeapp/src/main/java/com/cigelogistics/cigeapp/services/LocationService.java
+++ cigeapp/src/main/java/com/cigelogistics/cigeapp/services/LocationService.java
@@ -1,5 +1,38 @@
 package com.cigelogistics.cigeapp.services;
 
-public class LocationService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cigelogistics.cigeapp.models.Location;
+import com.cigelogistics.cigeapp.repositories.LocationRepository;
+
+
+@Service
+public class LocationService {
+	
+	@Autowired
+	private LocationRepository locationRepository;
+
+	public List<Location> findAll() {
+		return (List<Location>) locationRepository.findAll();
+	}
+
+	public Optional<Location> findById(Integer id) {
+		return locationRepository.findById(id);
+	}
+	
+	public void save(Location location) {
+		locationRepository.save(location);
+	}
+	
+	public void deleteById(Integer id) {
+		locationRepository.deleteById(id);
+	}
+
+	public List<Location> findByDescriptionContaining(String description) {
+		return null;
+	}	
 }
